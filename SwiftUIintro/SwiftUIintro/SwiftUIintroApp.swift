2f77a0db41b8b483c24528353a6e291aec6fbee9
Zachary <zachbrjensen@gmail.com>
2025-09-23 08:32:17 -0600

    Adding first practice app

---
+++ SwiftUIintro/SwiftUIintro/SwiftUIintroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIintroApp.swift
+//  SwiftUIintro
+//
+//  Created by Zachary Jensen on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIintroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
